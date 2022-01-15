35bea560dfd6a2c5c63491f17de833047c6c8692
pram15 <68314762+pram15@users.noreply.github.com>
2022-01-15 13:10:17 +0700

    Laravel
    Tugas laravel-tailwind vidio 1-19

---
+++ laravel-resto/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+        "./resources/views/*.blade.php",
+        "./resources/views/admin/*.blade.php",
+        "./resources/views/template/*.blade.php",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
